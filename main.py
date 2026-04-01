02ff59b99501d1f898d3104e8d33998c121b9582
AgarioTech <gromovdenis706@gmail.com>
2026-04-01 15:33:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,16 +13,3 @@ with open('math.csv', 'r', encoding='utf-8') as f:
         sorted_data = sorted(data.items(), key=lambda x: statistics.median(x[1]), reverse=True)
     result = [[student, statistics.median(values)] for student, value in sorted_data]
     print(tabulate(result, headers=['student', 'coffee_spent'], tablefmt='grid'))
-
-
-with open('math.csv', 'r', encoding='utf-8') as f:
-    data = {}
-    reader = csv.DictReader(f)
-    for row in reader:
-        data.setdefault(row['student'], []).append(int(row['coffee_spent']))
-
-    for student, values in data.items():
-        median = statistics.median(values)
-        sorted_data = sorted(data.items(), key=lambda x: statistics.median(x[1]), reverse=True)
-    result = [[student, statistics.median(values)] for student, value in sorted_data]
-    print(tabulate(result, headers=['student', 'coffee_spent'], tablefmt='grid'))
